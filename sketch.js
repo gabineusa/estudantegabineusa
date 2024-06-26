f336fbd8d8af8ff48987fb2b2298e131c793a75a
gabineusa <00001110030216sp@al.educacao.sp.gov.br>
2024-06-26 07:48:51 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,106 @@
+function draw() {
+    background(0);
+    mostraBolinha();
+    movimentaBolinha();
+    verificaColisaoBorda();
+    movimentaMinhaRaquete();
+    verificaColisaoRaquete(xRaquete, yRaquete);
+    mostraRaquete(xRaquete, yRaquete);
+    mostraRaquete(xRaqueteOponente, yRaqueteOponente);
+    movimenteRaqueteOponente();
+    verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente)
+   incluiPlacar(); 
+    marcaPonto();
+    
+   
+    
+   
+  }
+  
+  function mostraBolinha(){
+    circle(xBolinha, yBolinha, diametro);
+  }
+  
+  function movimentaBolinha(){
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;
+  }
+  
+  function verificaColisaoBorda(){
+    if (xBolinha + raio> width ||
+       xBolinha - raio< 0){
+      velocidadeXBolinha *= -1;
+    }
+    if (yBolinha + raio> height ||
+       yBolinha - raio < 0){
+      velocidadeYBolinha *= -1;
+    }
+  }
+  function mostraRaquete(x,y){
+  rect (x, y, raqueteComprimento, raqueteAltura)
+  }
+  
+  
+  
+  function movimentaRaqueteOponente() {
+      if (keyIsDown(87)) {
+          yRaqueteOponente -= 10;
+      }
+      if (keyIsDown(83)) {
+          yRaqueteOponente += 10;
+  
+      }
+  }
+  
+  function movimentaMinhaRaquete() {
+      if (keyIsDown(UP_ARROW)) {
+          yRaquete -= 10;
+      }
+      if (keyIsDown(DOWN_ARROW)) {
+          yRaquete += 10;
+     
+    }
+      }
+  
+    function verificaColisaoRaquete(x, y) {
+      colidiu = collideRectCircle(x, y, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
+      if (colidiu) {
+          velocidadeXBolinha *= -1;
+        raquetada.play()
+      }
+    }
+  
+  function  movimenteRaqueteOponente(){
+   velocidadeYOponente = yBolinha - yRaqueteOponente - raqueteComprimento /2 -30;
+    yRaqueteOponente += velocidadeYOponente
+    
+  }
+  function incluiPlacar(){
+      stroke(255);
+      textAlign(CENTER);
+      textSize(16);
+      fill(color(255, 140, 0));
+      rect(150, 10, 40, 20);
+      fill(255);
+      text(meusPontos, 170, 26);
+      fill(color(255, 140, 0));
+      rect(450, 10, 40, 20);
+      fill(255);
+      text(pontosDoOponente, 470, 26);
+  }
+  
+  
+  function marcaPonto() {
+      if (xBolinha > 591) {
+          meusPontos += 1;
+        ponto.play()
+      }
+      if (xBolinha < 9) {
+          pontosDoOponente += 1;
+        ponto.play()
+      }
+  }
+    
+  
+    
+   
